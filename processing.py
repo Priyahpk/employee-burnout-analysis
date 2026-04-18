ee2ecc1f97bd6638e1799581e5338bd5262231a2
HARIPRIYA K <khpriya03@gmail.com>
2026-04-19 00:12:22 +0530

    Add files via upload

---
+++ processing.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def load_data():
+    df = pd.read_csv("Palo Alto Networks.csv")
+    return df
+
+def preprocess(df):
+    df['OverTime'] = df['OverTime'].map({'Yes': 1, 'No': 0})
+    return df
+
+def create_engagement_index(df):
+    df['EngagementIndex'] = (
+        df['JobInvolvement'] +
+        df['JobSatisfaction'] +
+        df['EnvironmentSatisfaction'] +
+        df['RelationshipSatisfaction']
+    ) / 4
+    return df
+
+def create_burnout_risk(df):
+    def risk(row):
+        if row['OverTime'] == 1 and row['WorkLifeBalance'] <= 2:
+            return 'High'
+        elif row['OverTime'] == 1 or row['WorkLifeBalance'] <= 2:
+            return 'Medium'
+        else:
+            return 'Low'
+
+    df['BurnoutRisk'] = df.apply(risk, axis=1)
+    return df
